efa84be649ed4f90b78955532676d6705b694359
Ravenstudios <ravenstudiosrob@yahoo.com>
2022-03-18 10:19:14 -0500

    Added fifties and and few ui changes'

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ import tkinter as tk
 from tkinter import ttk
 # from tkinter.messagebox import showinfo
 
-denominations = ["Hundreds", "Twentys", "Tens", "Fives", "Ones", "Rolls", "Quarters", "Dimes", "Nickels", "Pennies"]
-denom_values = [100, 20, 10, 5, 1, 1, 0.25, 0.1, 0.05, 0.01]
+denominations = ["Hundreds", "Fifties", "Twentys", "Tens", "Fives", "Ones", "Rolls", "Quarters", "Dimes", "Nickels", "Pennies"]
+denom_values = [100, 50, 20, 10, 5, 1, 1, 0.25, 0.1, 0.05, 0.01]
 labels = []
 entries = []
 values = []
 
 total = 0
 index = 0
-text_entry_length = 7
+TEXT_ENTRY_LENGTH = 7
 
 # root window
 root = tk.Tk()
-root.geometry("300x650")
+root.geometry("450x650")
 root.title('Drawer Cash Counter')
 
 frame = ttk.Frame(root)
 frame.pack(padx=10, pady=10, fill='x', expand=True)
 
-total_entry = ttk.Entry(frame, textvariable=total, width = text_entry_length)
-error_label = ttk.Label(frame, text = "", font=("Arial", 25))
-error_label.config(foreground="red")
+total_entry = ttk.Entry(
+    frame,
+    textvariable=total,
+    width = TEXT_ENTRY_LENGTH,
+    justify = "right"
+    )
+
+directions = "Enter the quantity of bills and change. Rolls use the exact dollar ammount."
+
+error_label = ttk.Label(frame, text = directions, font=("Arial", 12))
+error_label.config(foreground="black")
 
 def get_total():
     global index, error_label
@@ -33,13 +41,17 @@ def get_total():
 
         except Exception as e:
             entries[i].focus()
+            entries[i].delete(0, tk.END)
+            entries[i].insert(0, "0")
             entries[i].selection_range(0, tk.END)
+            error_label.config(foreground="red")
             error_label["text"] = "Must enter a number"
             return
 
 
     index += 1
-    error_label["text"] = ""
+    error_label.config(foreground="black")
+    error_label["text"] = directions
     total_entry.delete(0, tk.END)
     total_entry.insert(0, total)
 
@@ -61,12 +73,12 @@ def handle_click(e):
 
 
 def setup():
-    error_label.pack()
+    error_label.pack(pady=(20, 20))
 
-    sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
+    # sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
 
     for i in range(len(denominations)):
-        if i == 5 or i == 6:
+        if i == 6 or i == 7:
             sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
 
         labels.append(ttk.Label(frame, text=denominations[i]))
@@ -77,17 +89,22 @@ def setup():
         entries.append(ttk.Entry(
             frame,
             textvariable = values[i],
-            width = text_entry_length,
+            width = TEXT_ENTRY_LENGTH,
             justify = "right"
             ))
 
         entries[i].bind("<ButtonPress-1>", handle_click)
-        entries[i].insert(0, "0")
+        if i == 6:
+            entries[i].insert(0, "0.00")
+        else:
+            entries[i].insert(0, "0")
+
         entries[i].pack()
 
     sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
 
-    total_label =ttk.Label(frame, text="Total")
+    total_label = ttk.Label(frame, text="Total")
+    total_entry.insert(0, "0.00")
     total_label.pack()
 
     total_entry.pack()
@@ -95,7 +112,7 @@ def setup():
     entries[0].focus()
     entries[0].selection_range(0, tk.END)
 
-    sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
+    # sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
 
 
 setup()
