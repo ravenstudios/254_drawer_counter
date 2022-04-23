22b360dfd9b9137c5851c7a0e557748e35a0a70d
Rob <ravensutdios@Users-MacBook-9.local>
2022-04-23 12:37:40 -0500

    Put drawer_counter into its own class and started tax_finder class

---
+++ drawer_counter.py
@@ -0,0 +1,124 @@
+import tkinter as tk
+from tkinter import ttk
+# from tkinter.messagebox import showinfo
+class Drawer_counter:
+    def __init__(self, root):
+        self.denominations = ["Hundreds", "Fifties", "Twentys", "Tens", "Fives", "Ones", "Rolls", "Quarters", "Dimes", "Nickels", "Pennies"]
+        self.denom_values = [100, 50, 20, 10, 5, 1, 1, 0.25, 0.1, 0.05, 0.01]
+        self.labels = []
+        self.entries = []
+        self.values = []
+
+        self.total = 0
+        self.index = 0
+        self.TEXT_ENTRY_LENGTH = 7
+
+        # root window
+
+
+        self.frame = ttk.Frame(root)
+        self.frame.pack(padx=10, pady=10, fill='x', expand=True)
+
+        self.total_entry = ttk.Entry(
+            self.frame,
+            textvariable=self.total,
+            width = self.TEXT_ENTRY_LENGTH,
+            justify = "right"
+            )
+
+        self.directions = "Enter the quantity of bills and change. Rolls use the exact dollar ammount."
+
+        self.error_label = ttk.Label(self.frame, text = self.directions, font=("Arial", 12))
+        self.error_label.config(foreground="black")
+
+        root.bind('<Return>', self.next_entry)
+
+
+    def invalid_number_error(self, entry):
+        entry.focus()
+        entry.delete(0, tk.END)
+        entry.insert(0, "0")
+        entry.selection_range(0, tk.END)
+        self.error_label.config(foreground="red")
+        self.error_label["text"] = "Must enter a number"
+
+
+
+
+
+
+    def next_entry(self, e):
+        print("ne")
+        self.entries[self.index % len(self.entries)].focus()
+        self.entries[self.index % len(self.entries)].selection_range(0, tk.END)
+        self.get_total()
+
+    def handle_click(self, e):
+        self.index
+        self.index = entries.index(e.widget)
+        self.entries[index].focus()
+        self.entries[index].selection_range(0, tk.END)
+        self.index += 1
+        print(self.index)
+
+
+    def setup(self):
+        self.error_label.pack(pady=(20, 20))
+
+        # sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
+
+        for i in range(len(self.denominations)):
+            if i == 6 or i == 7:
+                sep = ttk.Separator(self.frame, orient='horizontal').pack(fill = "x")
+
+            self.labels.append(ttk.Label(self.frame, text=self.denominations[i]))
+            self.labels[i].pack()
+
+            self.values.append(tk.StringVar())
+
+            self.entries.append(ttk.Entry(
+                self.frame,
+                textvariable = self.values[i],
+                width = self.TEXT_ENTRY_LENGTH,
+                justify = "right"
+                ))
+
+            self.entries[i].bind("<ButtonPress-1>", self.handle_click)
+            if i == 6:
+                self.entries[i].insert(0, "0.00")
+            else:
+                self.entries[i].insert(0, "0")
+
+            self.entries[i].pack()
+
+        self.sep = ttk.Separator(self.frame, orient='horizontal').pack(fill = "x")
+
+        self.total_label = ttk.Label(self.frame, text="Total")
+        self.total_entry.insert(0, "0.00")
+        self.total_label.pack()
+
+        self.total_entry.pack()
+
+        self.entries[0].focus()
+        self.entries[0].selection_range(0, tk.END)
+
+        # sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
+
+
+    def get_total(self):
+        self.index, self.error_label
+        total = 0
+        for i in range(len(self.entries)):
+            try:
+                total += float(self.entries[i].get()) * self.denom_values[i]
+
+            except Exception as e:
+                self.invalid_number_error(self.entries[i])
+                return
+
+
+        self.index += 1
+        self.error_label.config(foreground="black")
+        self.error_label["text"] = self.directions
+        self.total_entry.delete(0, tk.END)
+        self.total_entry.insert(0, total)
--- main.py
+++ main.py
@@ -1,120 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+import drawer_counter
 # from tkinter.messagebox import showinfo
 
-denominations = ["Hundreds", "Fifties", "Twentys", "Tens", "Fives", "Ones", "Rolls", "Quarters", "Dimes", "Nickels", "Pennies"]
-denom_values = [100, 50, 20, 10, 5, 1, 1, 0.25, 0.1, 0.05, 0.01]
-labels = []
-entries = []
-values = []
 
-total = 0
-index = 0
-TEXT_ENTRY_LENGTH = 7
 
-# root window
 root = tk.Tk()
 root.geometry("450x650")
 root.title('Drawer Cash Counter')
 
-frame = ttk.Frame(root)
-frame.pack(padx=10, pady=10, fill='x', expand=True)
+dc = drawer_counter.Drawer_counter(root)
 
-total_entry = ttk.Entry(
-    frame,
-    textvariable=total,
-    width = TEXT_ENTRY_LENGTH,
-    justify = "right"
-    )
+dc.setup()
 
-directions = "Enter the quantity of bills and change. Rolls use the exact dollar ammount."
-
-error_label = ttk.Label(frame, text = directions, font=("Arial", 12))
-error_label.config(foreground="black")
-
-def get_total():
-    global index, error_label
-    total = 0
-    for i in range(len(entries)):
-        try:
-            total += float(entries[i].get()) * denom_values[i]
-
-        except Exception as e:
-            entries[i].focus()
-            entries[i].delete(0, tk.END)
-            entries[i].insert(0, "0")
-            entries[i].selection_range(0, tk.END)
-            error_label.config(foreground="red")
-            error_label["text"] = "Must enter a number"
-            return
-
-
-    index += 1
-    error_label.config(foreground="black")
-    error_label["text"] = directions
-    total_entry.delete(0, tk.END)
-    total_entry.insert(0, total)
-
-
-
-def next_entry(e):
-
-    entries[index % len(entries)].focus()
-    entries[index % len(entries)].selection_range(0, tk.END)
-    get_total()
-
-def handle_click(e):
-    global index
-    index = entries.index(e.widget)
-    entries[index].focus()
-    entries[index].selection_range(0, tk.END)
-    index += 1
-    print(index)
-
-
-def setup():
-    error_label.pack(pady=(20, 20))
-
-    # sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
-
-    for i in range(len(denominations)):
-        if i == 6 or i == 7:
-            sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
-
-        labels.append(ttk.Label(frame, text=denominations[i]))
-        labels[i].pack()
-
-        values.append(tk.StringVar())
-
-        entries.append(ttk.Entry(
-            frame,
-            textvariable = values[i],
-            width = TEXT_ENTRY_LENGTH,
-            justify = "right"
-            ))
-
-        entries[i].bind("<ButtonPress-1>", handle_click)
-        if i == 6:
-            entries[i].insert(0, "0.00")
-        else:
-            entries[i].insert(0, "0")
-
-        entries[i].pack()
-
-    sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
-
-    total_label = ttk.Label(frame, text="Total")
-    total_entry.insert(0, "0.00")
-    total_label.pack()
-
-    total_entry.pack()
-
-    entries[0].focus()
-    entries[0].selection_range(0, tk.END)
-
-    # sep = ttk.Separator(frame, orient='horizontal').pack(fill = "x")
-
-
-setup()
-root.bind('<Return>', next_entry)
 root.mainloop()
